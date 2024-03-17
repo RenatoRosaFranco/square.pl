fa649f5bd03a6d994ffc030d6b8c7e5803734af8
Renato Franco <renato_ny@live.com>
2024-03-17 00:23:55 -0300

    feature: removing useless folders.

--- url-shortener/src/app/pages/api/shorten.js
+++
@@ -1,8 +0,0 @@
-import { nanoid } from 'nanoid';
-
-export default async (req, res) => {
-  if (req.method === 'POST') {
-    const { longUrl } = req.body;
-    const { shortId } = nanoid(8);
-  }
-}
